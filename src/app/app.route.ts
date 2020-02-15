89c4989232095ce832a67511c92446a4afd7280a
nileskh2504 <nileshfbk123@gmail.com>
2020-02-15 16:42:10 +0530

    French and German translation is covered now

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -10,7 +10,9 @@ const routes: Routes = [
 
 @NgModule({
     imports: [
-        RouterModule.forRoot(routes)
+        RouterModule.forRoot(routes, {
+            anchorScrolling: 'enabled'
+        })
     ],
     exports: [
         RouterModule
